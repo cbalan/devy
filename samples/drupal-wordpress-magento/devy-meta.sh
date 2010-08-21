6b012732ab00d06f0c1ba77d75565cf6b2ddc1c7
Catalin Balan <catalin.balan@gmail.com>
2010-08-21 17:31:21 +0300

    Added magento local.xml sample

--- samples/drupal-wordpress-magento/devy-meta.sh
+++ samples/drupal-wordpress-magento/devy-meta.sh
@@ -2,10 +2,11 @@
 # devy-meta.sh sample
 #
 
-PACKAGE_PREFIX=drupal-magento-devy-sample
+PACKAGE_PREFIX=drupal-wordpress-magento
 [ -z "${PACKAGE_VERSION}" ] && PACKAGE_VERSION=$(date +%Y%m%d%H%M)
 
-DEVY_BUILD_DRUSH_MAKE_FILE="${DEVY_HOME}/drupal/modules/drupal-magento-devy-sample.make"
+DEVY_BUILD_DRUSH_MAKE_FILE="${DEVY_HOME}/drupal/modules/drupal-magento.make"
+DEVY_BUILD_OVERLAY_WORDPRESS="http://wordpress.org/latest.tar.gz"
 DEVY_BUILD_OVERLAY_MAGENTO="svn:http://svn.magentocommerce.com/source/branches/1.4/ ${DEVY_HOME}/magento"
 
 # Installation target for each component
